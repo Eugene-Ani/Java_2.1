9f3ca386d66a711162b72026b042c484b579a277
Eugene-Ani <deltasvet@gmail.com>
2021-12-05 14:37:13 +0300

    Добавил код программы для расчета количества миль

---
+++ Main.java
@@ -0,0 +1,10 @@
+    public class Main {
+        public static void main(String[] args) {
+            long ticketPrice = 7000;
+            long amountMoney = 20;
+
+            long amountMiles = ticketPrice / amountMoney;
+            System.out.println("Вам положено " + amountMiles + " миль");
+            System.out.println("made by Dad, enjoy!");
+        }
+    }
